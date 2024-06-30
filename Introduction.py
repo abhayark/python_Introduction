b8ec006cc258a3be9d263ea6a8a726e44aa8cb51
abhayark <pcabhay0@gmail.com>
2024-06-30 17:29:35 +0530

    Initial commit

---
+++ Introduction.py
@@ -0,0 +1,27 @@
+#Python Math:
+
+#addition(+):
+print(2 + 3)
+  
+#subtraction(-)
+print(3 - 1)   
+
+#multiplication(*)
+print(2 * 3)   
+
+#division(/)
+print(3 / 2)   
+
+#exponential(**)
+print(3 ** 2)  
+
+#modulus(%)
+print(3 % 2)   
+
+# Floor division operator()
+print(3 // 2)  
+
+
+#type function:
+
+print(type(10))
